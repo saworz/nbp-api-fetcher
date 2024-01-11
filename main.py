7b5afcd1f8723220c78fd3a6220cbe044b99ef4a
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-11 19:57:16 +0100

    Set scheduler time

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from nbp_api import fetch_nbp_api
 
 scheduler = BackgroundScheduler()
 job = scheduler.add_job(fetch_nbp_api, trigger="cron",
-                        day="*", hour="19", minute="53", second="20", max_instances=1)
+                        day="*", hour="00", minute="00", max_instances=1)
 
 if __name__ == "__main__":
     fetch_nbp_api()
