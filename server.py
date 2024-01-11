d30b46b77bd2ca2d510b9038eef84be7b291ce59
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-11 17:12:23 +0100

    Queried exchange rates as response for get_response_rates endpoint

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 import pandas as pd
 
+from typing import List, Dict
 from main import ALL_CURRENCY_CSV_FILENAME
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-def read_exchange_rates():
+def read_exchange_rates(requested_currencies: List[str]) -> Dict:
     df = pd.read_csv(ALL_CURRENCY_CSV_FILENAME)
-    exchange_rates = df.to_dict()
-    return exchange_rates
+    df.set_index("Date", inplace=True)
+    filtered_df = df.filter(requested_currencies)
+    return filtered_df.to_dict()
 
 
 @app.route("/api/get_exchange_rates/", methods=["GET"])
 def get_exchange_rates():
-    exchange_rates = read_exchange_rates()
     requested_currencies = request.args.getlist("currencies")
 
     if not requested_currencies:
         return {"message": "No currencies to query received"}, 404
 
-    return {"message": "placeholder"}, 200
+    exchange_rates = read_exchange_rates(requested_currencies)
+    return {"message": "CSV file queried successfully", "exchange_rates": exchange_rates}, 200
