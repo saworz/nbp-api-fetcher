badd76924172f0a2635ccd6d63944961966020eb
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-12 20:07:24 +0100

    Downloading exchange rates as csv file

--- frontend/src/api/FetchExchangeRates.js
+++ frontend/src/api/FetchExchangeRates.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 export const fetchExchangeRates = async (selectedCurrencies) => {
-  console.log("IN fetching")
-  console.log(selectedCurrencies)
   const apiUrl = "http://127.0.0.1:5000/api/get_exchange_rates/";
   const queryParams = selectedCurrencies.map(query => `currencies=${query}`).join('&');
   const finalUrl = `${apiUrl}?${queryParams}`;
 
-  console.log(finalUrl)
   try {
     const response = await axios.get(finalUrl);
-    return response.data
+    const jsonValidString = response.data.replace(/:\s*NaN/g, ': null');
+    return JSON.parse(jsonValidString)['exchange_rates'];
   } catch (error) {
     console.error("Error fetching data:", error)
   }
--- frontend/src/components/MainPage/PreviewButton/PreviewButton.js
+++ frontend/src/components/MainPage/PreviewButton/PreviewButton.js
@@ -5,11 +5,34 @@ import fetchExchangeRates from "../../../api/FetchExchangeRates";
 export const PreviewButton = ({ selectedCurrencies }) => {
   const [exchangeRates, setExchangeRates] = useState();
 
-  useEffect(() => {
-    if (exchangeRates) {
-      console.log(exchangeRates)
+  const downloadExchangeRatesAsCSV = () => {
+    const csvData = [];
+    for (const currency in exchangeRates) {
+      const rates = exchangeRates[currency];
+      const rowData = { Date: currency };
+
+      for (const date in rates) {
+        rowData[date] = rates[date];
+      }
+
+      csvData.push(rowData);
     }
 
+    const csvContent = Object.keys(csvData[0]).map(key => key + ',' + csvData.map(row => row[key]).join(',')).join('\n');
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'exchangeRates.csv';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  useEffect(() => {
+    if (exchangeRates) {
+      downloadExchangeRatesAsCSV()
+    }
   }, [exchangeRates]);
 
   const fetchData = async () => {
@@ -22,13 +45,12 @@ export const PreviewButton = ({ selectedCurrencies }) => {
   };
 
   const handlePreviewClick = async () => {
-    // await fetchData();
-    console.log('click')
+    await fetchData();
   };
 
   return (
     <div>
-      <button onClick={handlePreviewClick}>Preview Exchange Rates</button>
+      <button onClick={handlePreviewClick}>Download as .CSV</button>
     </div>
   );
 };
