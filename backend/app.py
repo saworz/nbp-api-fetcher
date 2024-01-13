45cfa5a0233a9113d2451510cbcd78ed3aec197f
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-13 20:40:18 +0100

    Moved scheduler to src init

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,5 @@
-import logging
-
-from src import config, app
-from apscheduler.schedulers.background import BackgroundScheduler
+from src import config, app, scheduler
 from src.utils.fetch_job import fetch_nbp_api
-
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
-
-scheduler = BackgroundScheduler()
-job = scheduler.add_job(fetch_nbp_api, trigger="cron",
-                        day="*", hour="00", minute="00", max_instances=1)
 
 if __name__ == "__main__":
     """Main execution script"""
--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,11 +1,20 @@
+import logging
 from flask import Flask
 from flask_cors import CORS
 from .config.config import Config
+from apscheduler.schedulers.background import BackgroundScheduler
+from .utils.fetch_job import fetch_nbp_api
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 config = Config().dev_config
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(fetch_nbp_api, trigger="cron",
+                        day="*", hour="00", minute="00", max_instances=1)
 
 from .routes.routes import routes
 app.register_blueprint(routes)
