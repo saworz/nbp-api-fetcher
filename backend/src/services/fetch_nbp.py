ba09421d45d908cded6c5538763ddd4a97e6b6fd
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-15 11:33:41 +0100

    Changed error handling for fetching data

--- backend/src/services/fetch_nbp.py
+++ backend/src/services/fetch_nbp.py
@@ -12,32 +12,35 @@ class NbpFetcher(BaseModel):
     days_to_end: int
     currency_to_fetch: List[str]
 
-    def fetch_data(self, currency_name: str) -> List[Dict] | None:
+    def fetch_data(self, currency_name: str) -> List[Dict]:
         """Fetches data from nbp api and returns it as a list of dicts"""
         start_date = format_date(self.days_to_start)
         end_date = format_date(self.days_to_end)
         api_url = f"https://api.nbp.pl/api/exchangerates/rates/{self.table_type}/{currency_name}/{start_date}/{end_date}/"
 
-        try:
-            response = requests.get(api_url)
-            response.raise_for_status()
+        response = requests.get(api_url)
+        response.raise_for_status()
 
-            if response.status_code == 200:
-                data = response.json()
-                return data["rates"]
-            else:
-                logging.error(f"Error: Unable to fetch data. Response: {response.text}")
-                return
-        except requests.RequestException as e:
-            logging.error(f"An error occurred on the API request: {e}")
-        except ValueError as e:
-            logging.error(f"Error parsing JSON response: {e}")
+        if response.status_code == 200:
+            data = response.json()
+            return data["rates"]
+        else:
+            raise Exception(f"Unable to fetch data. Response: {response}")
 
     def get_fetched_rates(self) -> Dict[str, List[Dict]]:
         fetched_rates = {}
 
         for currency in self.currency_to_fetch:
-            rates = self.fetch_data(currency)
-            if rates:
-                fetched_rates[f"{currency.upper()}/PLN"] = rates
+            try:
+                rates = self.fetch_data(currency)
+                if rates:
+                    fetched_rates[f"{currency.upper()}/PLN"] = rates
+
+            except requests.RequestException as e:
+                logging.error(f"An error occurred on the API request: {e}")
+            except ValueError as e:
+                logging.error(f"Error parsing JSON response: {e}")
+            except Exception as e:
+                logging.error(f"An fetching data: {e}")
+
         return fetched_rates
