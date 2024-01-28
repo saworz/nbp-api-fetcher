f6bec0478828dc6e14a01f8cff975c33dc36d861
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-28 23:51:43 +0100

    Making async request calls

--- backend/src/services/fetch_nbp.py
+++ backend/src/services/fetch_nbp.py
@@ -1,5 +1,5 @@
-import requests
-import logging
+import asyncio
+import aiohttp
 from typing import List, Dict
 from backend.src import FetchConfig
 from backend.src.utils.format_date import format_date
@@ -14,38 +14,35 @@ class NbpFetcher:
         self.days_to_start = fetch_config.days_to_start
         self.days_to_end = fetch_config.days_to_end
         self.currency_to_fetch = fetch_config.currency_to_fetch
+        self.url_list = []
 
-    def fetch_data(self, currency_name: str) -> List[Dict]:
-        """Fetches data from nbp api and returns it as a list of dicts"""
+    def get_tasks(self, session):
+        """Creates tasks list for async execution"""
+        tasks = []
+        for url in self.url_list:
+            tasks.append(asyncio.create_task(session.get(url, ssl=False)))
+        return tasks
+
+    def get_urls(self):
+        """Creates list of api urls"""
         start_date = format_date(self.days_to_start)
         end_date = format_date(self.days_to_end)
 
-        api_parameters = f"{self.table_type}/{currency_name}/{start_date}/{end_date}/"
-        api_url = NBP_API_URL + api_parameters
+        for currency in self.currency_to_fetch:
+            api_parameters = f"{self.table_type}/{currency}/{start_date}/{end_date}/"
+            api_url = NBP_API_URL + api_parameters
+            self.url_list.append(api_url)
 
-        response = requests.get(api_url)
-        response.raise_for_status()
-
-        if response.status_code == 200:
-            data = response.json()
-            return data["rates"]
-        else:
-            raise Exception(f"Unable to fetch data. Response: {response}")
-
-    def get_fetched_rates(self) -> Dict[str, List[Dict]]:
+    async def fetch_data(self) -> Dict[str, List[Dict]]:
+        """Fetches data asynchronously"""
         fetched_rates = {}
 
-        for currency in self.currency_to_fetch:
-            try:
-                rates = self.fetch_data(currency)
-                if rates:
-                    fetched_rates[f"{currency.upper()}/PLN"] = rates
-
-            except requests.RequestException as e:
-                logging.error(f"An error occurred on the API request: {e}")
-            except ValueError as e:
-                logging.error(f"Error parsing JSON response: {e}")
-            except Exception as e:
-                logging.error(f"An fetching data: {e}")
+        self.get_urls()
+        async with aiohttp.ClientSession() as session:
+            tasks = self.get_tasks(session)
+            responses = await asyncio.gather(*tasks)
+            for currency, response in zip(self.currency_to_fetch, responses):
+                result = await response.json()
+                fetched_rates[f"{currency.upper()}/PLN"] = result["rates"]
 
         return fetched_rates
