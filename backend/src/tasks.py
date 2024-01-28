1fc32c71df855988e6630a1ef026ca4d75c099fd
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-28 22:42:42 +0100

    Made scheduler call async function

---
+++ backend/src/tasks.py
@@ -0,0 +1,14 @@
+from . import app, config, scheduler
+
+
+def flask_server() -> None:
+    app.run(
+        debug=config.DEBUG,
+        use_reloader=config.USE_RELOADER,
+        host=config.HOST,
+        port=config.PORT
+    )
+
+
+async def async_scheduler() -> None:
+    scheduler.start()
