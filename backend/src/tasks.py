562849b42a0151f671ae01914c2bffe9bcac8c4c
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-29 00:35:47 +0100

    Fixed config naming

--- backend/src/tasks.py
+++ backend/src/tasks.py
@@ -3,11 +3,11 @@ from . import app, config, scheduler
 
 def flask_server() -> None:
     app.run(
-        debug=config.DEBUG,
-        use_reloader=config.USE_RELOADER,
-        host=config.HOST,
-        port=config.PORT,
-        threaded=config.THREADED
+        debug=config.debug,
+        use_reloader=config.use_reloader,
+        host=config.host,
+        port=config.port,
+        threaded=config.threaded
     )
 
 
