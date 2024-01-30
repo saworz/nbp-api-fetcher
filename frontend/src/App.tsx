7c016419d28c793cea6898bd8f827d19f1384b6b
Michal Cz <michal.czechowicz7@gmail.com>
2024-01-30 23:03:42 +0100

    Added save to server button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MainPageView from './views/MainPageView';
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
--- frontend/src/components/Buttons/SaveButton/SaveButton.tsx
+++ frontend/src/components/Buttons/SaveButton/SaveButton.tsx
@@ -1,6 +1,7 @@
 import "./SaveButton.css"
 import { toast } from "react-toastify";
-
+import postRatesToSave from "api/postRatesToSave";
+import fetchAnalyzedData from "api/fetchAnalyzedData";
 
 interface SaveButtonProps {
   selectedCurrencies: string[];
@@ -8,9 +9,59 @@ interface SaveButtonProps {
 }
 
 export const SaveButton: React.FC<SaveButtonProps> = ({ selectedCurrencies, disabled }) => {
+
+  const postData = async () => {
+    try {
+      const response = await postRatesToSave(selectedCurrencies);
+
+      if (response.status === 200) {
+        toast.success(
+          `Data for ${selectedCurrencies.join(', ')} successfully saved to the server! ✔️`,
+          {theme: "dark"})
+      } else {
+        console.error("Failed to post data: ", response.status)
+        toast.error(
+          "Error occurred when saving data to the server ❌",
+          {theme: "dark"})
+      }
+    } catch (error) {
+      console.error("Error posting data: ", error)
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      return await fetchAnalyzedData(selectedCurrencies);
+    } catch (error) {
+      console.error("Error fetching analyzed data:", error)
+    }
+  };
+
+  const handleSavingClick = async () => {
+    await postData();
+    const analyzedData = await fetchData();
+
+    for (const currencyPair in analyzedData) {
+      const calculatedData = analyzedData[currencyPair]
+
+      toast.info(
+        <div><b>{currencyPair}</b> analyzed data<br />
+          Average value: {calculatedData['average_value']}<br />
+          Median value: {calculatedData['median_value']}<br />
+          Min value: {calculatedData['min_value']}<br />
+          Max value: {calculatedData['max_value']}<br />
+        </div>,
+        { position: "top-left", autoClose: false, draggable: false});
+    }
+  };
+  
   return (
     <div>
-      <button>Save to server</button>    
+      <button
+        onClick={handleSavingClick}
+        disabled={disabled}
+        className={disabled ? "disabled" : "enabled"}
+      >Save to server</button>
     </div>
   );
 };
